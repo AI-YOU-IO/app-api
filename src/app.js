9e17a33e3ede9bbf448ddebf0d8dc34d878bd011
Paul <89112861+PaulVega25@users.noreply.github.com>
2026-03-12 16:13:30 -0500

    hotfix: sandbox fixed
    hotfix sandbox fixed

--- src/app.js
+++ src/app.js
@@ -58,6 +58,7 @@ app.use(responseHandler);
 // Rutas publicas (sin auth)
 app.use("/api/crm", usuarioRoutes);
 app.use("/api/crm/tools", pagoRoutes);
+app.use("/api/sandbox", sandboxRoutes);
 app.post("/api/crm/tipificaciones", ConfiguracionController.createTipificacion);
 // Rutas protegidas del CRM (requieren auth)
 app.use("/api/crm", authMiddleware, auditoriaRoutes, configuracionRoutes, llamadaRoutes, tipificacionLlamadaRoutes, personaRoutes, whatsappRoutes, transcripcionRoutes, configuracionWhatsappRoutes);
