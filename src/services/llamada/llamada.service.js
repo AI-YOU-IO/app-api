be0605e0538aac7061eb9a11ff770cc56ad05170
Edwind Pineda <edwindpineda92@gmail.com>
2026-03-18 17:35:16 -0500

    act

--- src/services/llamada/llamada.service.js
+++ src/services/llamada/llamada.service.js
@@ -70,10 +70,13 @@ class LlamadaService {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
