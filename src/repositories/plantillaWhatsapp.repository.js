f2ba5eaef74db8e812f1d81711c9d51b9bb9d678
Diego <111472703+Kitaro1@users.noreply.github.com>
2026-03-27 17:05:31 -0500

    FIX: Corrigiendo nuevamente el inserid de plantillas

--- src/repositories/plantillaWhatsapp.repository.js
+++ src/repositories/plantillaWhatsapp.repository.js
@@ -62,7 +62,7 @@ class PlantillaWhatsappRepository {
 
     async create(data) {
         try {
-            const [, result] = await pool.execute(
+            const [result] = await pool.execute(
                 `INSERT INTO plantilla_whatsapp
                 (id_empresa, name, status, category, "language", header_type, header_text, body, footer, buttons, url_imagen, meta_template_id, id_formato, estado_registro, usuario_registro)
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 1, ?)`,
@@ -92,7 +92,7 @@ class PlantillaWhatsappRepository {
 
     async update(id, data) {
         try {
-            const [, result] = await pool.execute(
+            const [result] = await pool.execute(
                 `UPDATE plantilla_whatsapp
                 SET name = ?, status = ?, category = ?, "language" = ?,
                     header_type = ?, header_text = ?, body = ?, footer = ?,
@@ -156,7 +156,7 @@ class PlantillaWhatsappRepository {
 
     async delete(id, usuario_actualizacion = null) {
         try {
-            const [, result] = await pool.execute(
+            const [result] = await pool.execute(
                 `UPDATE plantilla_whatsapp SET estado_registro = 0, usuario_actualizacion = ?, fecha_actualizacion = CURRENT_TIMESTAMP WHERE id = ?`,
                 [usuario_actualizacion, id]
             );
